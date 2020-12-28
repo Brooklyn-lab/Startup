f20d4a2071c36b2c5ad5c5170a19e7bbfba682a2
Brooklyn-lab <61649133+Brooklyn-lab@users.noreply.github.com>
2020-12-28 15:34:32 +0200

    add animate-hidden

--- src/main.js
+++ src/main.js
@@ -1,70 +1,64 @@
 jQuery(document).ready(function () {
+  let burger = $('.header__burger');
+  let btns = $('.works__btn');
 
-  let burger = $(".header__burger");
-  let btns = $(".works__btn");
-
-  $("#menu").on("click","a", function (event) {
+  $('#menu').on('click', 'a', function (event) {
     event.preventDefault();
-    let id  = $(this).attr('href'),
-        top = $(id).offset().top;
-    $('body,html').animate({scrollTop: top}, 1500);
+    let id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
   });
 
-  let all = $(".header__overlay, .header__logo-link, .header__button, .menu");
+  let all = $('.header__overlay, .header__logo-link, .header__button, .menu');
 
-  burger.on("click", function() {
-    all.toggleClass("active");
-  })
+  burger.on('click', function () {
+    all.toggleClass('active');
+  });
 
   $('.about__slider').slick({
-      infinite: true,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      speed: 300,
-      responsive: [
-          {
-            breakpoint: 992,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 1,
-            }
-          },
-          {
-            breakpoint: 768,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 576,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    speed: 300,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 
-  btns.on("click", function () {
-    $(this)
-        .addClass("active")
-        .siblings()
-        .removeClass("active");
-    let cards = $(".works__card");
-    cards.removeClass("active");
-    let btnText = $(this)
-        .text()
-        .toLowerCase();
+  btns.on('click', function () {
+    $(this).addClass('active').siblings().removeClass('active');
+    let cards = $('.works__card');
+    cards.removeClass('active');
+    let btnText = $(this).text().toLowerCase();
     let final = cards.filter(function () {
-        if (btnText === "all") {
-            return cards;
-        } else {
-            return $(this).data("category") === btnText;
-        }
+      if (btnText === 'all') {
+        return cards;
+      } else {
+        return $(this).data('category') === btnText;
+      }
     });
     final.each(function () {
-        $(this).addClass("active");
+      $(this).addClass('active');
     });
   });
 
@@ -76,38 +70,56 @@ jQuery(document).ready(function () {
     slidesToShow: 1,
     slidesToScroll: 1,
     accessibility: false,
-    speed: 300
+    speed: 300,
   });
 
   //-------------footer-date------------------------
   let date = new Date().getFullYear();
-  $("#date").text(new Date().getFullYear());
+  $('#date').text(new Date().getFullYear());
 
-  $("#ancor").on("click","a", function (event) {
+  $('#ancor').on('click', 'a', function (event) {
     event.preventDefault();
-    let id  = $(this).attr('href'),
-        top = $(id).offset().top;
-    $('body,html').animate({scrollTop: top}, 1500);
+    let id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
   });
+
+  (function showBlocksOnScroll() {
+    if ($.fn.waypoint) {
+      $('.animate-hidden').addClass('not-in-view');
+      $('.animate-hidden').waypoint(
+        function () {
+          this.element.classList.remove('not-in-view');
+        },
+        { offset: '80%' },
+      );
+    }
+  })();
+
+  //   let animateHidden = $('.animate-hidden');
+  //   animateHidden.waypoint(
+  //     function () {
+  //       animateHidden.addClass('not-in-view');
+  //     },
+  //     { offset: '80%' },
+  //   );
 });
 
-$(window).scroll(function() {
+$(window).scroll(function () {
   let height = $(window).scrollTop();
-   /*Если сделали скролл на 600px задаём новый класс для span*/
-  if(height > 600){
-  $('#span').addClass('active');
-  } else{
-  /*Если меньше 600px удаляем класс для span*/
-  $('#span').removeClass('active');
+  /*Если сделали скролл на 600px задаём новый класс для span*/
+  if (height > 600) {
+    $('#span').addClass('active');
+  } else {
+    /*Если меньше 600px удаляем класс для span*/
+    $('#span').removeClass('active');
   }
 
-  if(height > 100){
-  /*Если сделали скролл на 100px задаём новый класс для ancor*/
-  $('#ancor').addClass('active');
-  } else{
-  /*Если меньше 100px удаляем класс для ancor*/
-  $('#ancor').removeClass('active');
+  if (height > 100) {
+    /*Если сделали скролл на 100px задаём новый класс для ancor*/
+    $('#ancor').addClass('active');
+  } else {
+    /*Если меньше 100px удаляем класс для ancor*/
+    $('#ancor').removeClass('active');
   }
-
 });
-
